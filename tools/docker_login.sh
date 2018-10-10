8c267238578b0109e0e2f32eab95080ab504ea43
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-10-10 13:50:45 +0200

    Remove incorrect variable check in docker login script

--- tools/docker_login.sh
+++ tools/docker_login.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-if [ -z $CLOUD_SERVICE_KEY ]
-then
-    echo "$CLOUD_SERVICE_KEY needs to be set in order to activate service account";
-    exit 1;
-fi
-
 echo $GCLOUD_SERVICE_KEY > ${HOME}/account-auth.json
 
 cat ${HOME}/account-auth.json | docker login -u _json_key --password-stdin https://eu.gcr.io
