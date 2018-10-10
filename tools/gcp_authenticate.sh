39fab79753eeaf1da2d7cb7778733e7edbfda37a
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-10-10 12:37:14 +0200

    Remove parameter check as set -euo pipefail helps with this

--- tools/gcp_authenticate.sh
+++ tools/gcp_authenticate.sh
@@ -4,11 +4,5 @@
 
 set -euo pipefail
 
-if [ -z $CLOUD_SERVICE_KEY ]
-then
-    echo "$CLOUD_SERVICE_KEY needs to be set in order to activate service account";
-    exit 1;
-fi
-
 echo $GCLOUD_SERVICE_KEY > ${HOME}/account-auth.json
 gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
