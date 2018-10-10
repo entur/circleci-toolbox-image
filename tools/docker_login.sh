90dc906c7baf7b4c8312979d3c52288e0e261238
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-10-10 14:01:15 +0200

    Debug docker login

--- tools/docker_login.sh
+++ tools/docker_login.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 echo $GCLOUD_SERVICE_KEY > ${HOME}/account-auth.json
-
+wc -l ${HOME}/account-auth.json
 cat ${HOME}/account-auth.json | docker login -u _json_key --password-stdin https://eu.gcr.io
