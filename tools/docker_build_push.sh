a8c844fa0af587c538eb2ab4a8858c7e9a652f43
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-10-17 11:10:09 +0200

    Try to get around an issue with build args being double quoted

--- tools/docker_build_push.sh
+++ tools/docker_build_push.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 set -euo pipefail
+set -o xtrace
 
 CLOUDSDK_CORE_PROJECT="carbon-1287"
 IMAGE="eu.gcr.io/${CLOUDSDK_CORE_PROJECT}/${CIRCLE_PROJECT_REPONAME}"
 echo "Image: ${IMAGE}"
 
-docker build -t "${IMAGE}" ${BUILD_ARGS:-} .
+eval "docker build -t ${IMAGE} ${BUILD_ARGS} ."
+
 docker tag "${IMAGE}" "${IMAGE}":"${CIRCLE_SHA1}"
 docker push "${IMAGE}"
