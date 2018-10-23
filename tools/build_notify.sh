cabf8cb2566cb0060a999773f6373fba6f11c3ac
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-10-23 09:27:31 +0200

    Use curl --fail when sending build status to babylon to produce non zero exit code, being able to detect failures more easily

--- tools/build_notify.sh
+++ tools/build_notify.sh
@@ -14,4 +14,4 @@ fi
 
 echo "{\"name\": \"${CIRCLE_PROJECT_REPONAME}\", \"build\": {\"status\": \"SUCCESS\", \"phase\": \"FINALIZED\", \"scm\": {\"commit\": \"${CIRCLE_SHA1}\"}}}" > message.json &&
 echo "sending: $(cat message.json) to ${BUILD_STATUS_URL}" &&
-curl -X POST --data @message.json -H "apiKey: ${BUILD_STATUS_API_KEY}" -H "Content-Type: application/json" -H "ET-Client-Name: CircleCI" -H "ET-Client-ID: CircleCI-${CIRCLE_PROJECT_REPONAME}"  "${BUILD_STATUS_URL}";
+curl --fail -X POST --data @message.json -H "apiKey: ${BUILD_STATUS_API_KEY}" -H "Content-Type: application/json" -H "ET-Client-Name: CircleCI" -H "ET-Client-ID: CircleCI-${CIRCLE_PROJECT_REPONAME}"  "${BUILD_STATUS_URL}";
